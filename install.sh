36ff57c887789292b9dbc41f0e8a38d51c83a37b
Maximilian Rudolph <rudolph-max@gmx.net>
2024-03-28 13:33:25 +0000

    using pkglist to install packages with yay & hook into pacman

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo "-- Packages --"
+
+echo "Installing yay..."
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git ~/yay
+pushd ~/yay
+makepkg -si
+popd
+rm -rf ~/yay
+
+pushd ~/dotfiles
+
+echo "Installing packages with yay..."
+yay -S --needed < ~/dotfiles/pkglist.txt
+popd
+
+echo "Installing Pacman auto-update pkglist hook..."
+sudo mkdir /etc/pacman.d/hooks
+sudo cp update_pkglist.hook /etc/pacman.d/hooks/
