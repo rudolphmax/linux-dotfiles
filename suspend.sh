39402964a93e4097b23ea631cdb118f8fac03adb
Maximilian Rudolph <rudolph-max@gmx.net>
2024-03-31 17:29:20 +0200

    fixed suspend / idle

--- suspend.sh
+++ suspend.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+interval=150
+
 swayidle -w \
-timeout 120 ' swaylock ' \
-timeout 400 ' hyprctl dispatch dpms off' \
-timeout 12000 'systemctl suspend' \
-resume ' hyprctl dispatch dpms on' \
-before-sleep 'swaylock'
+  timeout $interval 'swaylock -f -c 000000' \
+  timeout $((interval*2)) 'hyprctl dispatch dpms off' \
+  timeout $((interval*3)) 'systemctl suspend' \
+    resume ' hyprctl dispatch dpms on' \
+  before-sleep 'swaylock -f -c 000000'
 
